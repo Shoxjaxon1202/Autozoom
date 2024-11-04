ab61ef80d1961e9fa5f6b304fb54f394321653df
Odilbek-prog <odilbeknormuhamedov7@gmail.com>
2024-11-04 15:37:32 +0500

    serv and footer

--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import "../styles/home.scss";
 import BudgetCars from "../components/UI/Cars/BudgetCars/BudgetCars";
+import Services from "../components/UI/Services/Services";
 const Home = () => {
   return (
     <div className="home">
       {/* <h2>Home page</h2> */}
       <BudgetCars />
+      <Services />
     </div>
   );
 };
