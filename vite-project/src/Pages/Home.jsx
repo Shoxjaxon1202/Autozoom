33a250291e707fcb6373d6e85a72ebcbae3d64c0
ErgashMagdi <abdulayevan701@gmail.com>
2024-11-08 17:50:18 +0500

    intro

--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "../styles/home.scss";
 import Services from "../components/UI/Services/Services";
 import Cars from "../components/UI/Car/Cars";
+import Intro from "../components/UI/Intro/Intro";
 
 const Home = () => {
   return (
     <div className="home">
+      <Intro/>
       <Cars />
       <Services />
     </div>
